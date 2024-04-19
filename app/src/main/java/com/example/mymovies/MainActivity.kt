e700b7c08ed30e4fa583af8e382ac4c6d9ff3e0c
Anil Khanal <66034315+makzibs@users.noreply.github.com>
2024-04-19 15:32:44 +0545

    fixed scaffold error

--- app/src/main/java/com/example/mymovies/MainActivity.kt
+++ app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -17,7 +21,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Magenta
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mymovies.ui.theme.MyMoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,25 +46,31 @@ class MainActivity : ComponentActivity() {
 fun MyApp(content: @Composable () -> Unit) {
     MyMoviesTheme {
         // A surface container using the 'background' color from the theme
-    Scaffold (topBar = {
-        TopAppBar(
-            title = { Text("Movies") },
-            colors = TopAppBarDefaults.topAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                titleContentColor = MaterialTheme.colorScheme.primary,
-            ),
-        )
-    }) {
-        content()
-    }
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Movies") },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary,
+                    ),
+                )
+            }
+        ) {
+            Column(modifier = Modifier.padding(it)) {
+                content()
+            }
+        }
     }
 }
 
 @Composable
-fun MainContent() {
-  Surface(color = MaterialTheme.colorScheme.background) {
-      Text(text = "Hello")
-  }
+fun MainContent(movieList: List<String> = listOf("Animal", "DDLJ", "Animal", "DDLJ", "Life of Pie")) {
+    LazyColumn {
+        items(items = movieList) {
+            Text(text = it)
+        }
+    }
 }
 
 
